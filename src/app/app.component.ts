b4172fab655f046ca27ecd6f594af0dc2d5f359b
Omkar Chavan <69120575+osc99@users.noreply.github.com>
2022-06-12 01:32:01 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  /*template: `<h1 style="color:Blue;">Marvellous Infosystem</h1>
+  <p>Input Text field is : <input type="text" id="name" name="name"></p>
+  `*/
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment5';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewcomponentComponent } from './newcomponent/newcomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewcomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
